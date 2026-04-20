c1acf45bb131b36950dae166ae83891d2ff1ba15
iVanov <ivanov@iVanovs-MacBook-Air.local>
2026-04-19 23:09:14 -0400

    feat: FLAB-11 subtask 1

---
+++ lib/lesson_11/homework_11_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Screen extends StatelessWidget {
+  const Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 150,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+      ),
+    );
+  }
+}
